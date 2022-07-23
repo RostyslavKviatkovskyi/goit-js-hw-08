58e63ca4d4d87bb81b07c6673964d4f523d5ba18
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-07-23 19:25:54 +0300

    first task + second task(beginning)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const photosMarkup = createPhotosMarkup(galleryItems);
+
+function createPhotosMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">  
+    <a class="gallery__item" href="${original}">
+      <img 
+        class="gallery__image" 
+        src="${preview}"
+        alt="${description}" />
+    </a>
+    </li>
+    `;
+    })
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML('beforeend', photosMarkup);
+
+const gallery = new SimpleLightbox('.gallery__item', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
