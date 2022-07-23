c91b999df1b8d95d0819708855ca3c94a8ce7c97
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-07-23 20:05:51 +0300

    task_2_stable_version

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,9 +3,30 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.on('timeupdate', function (data) {
+  // console.log('played the video!');
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
 });
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
