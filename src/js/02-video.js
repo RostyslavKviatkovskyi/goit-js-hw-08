b29a1181dbf73b6df89f096a844113de446cc646
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-07-23 20:29:26 +0300

    task_2_throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,19 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  // console.log('played the video!');
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    // console.log('played the video!');
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }),
+  1000
+);
 
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
